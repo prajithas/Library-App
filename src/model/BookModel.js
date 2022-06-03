93df9c984d2b0f55d6643dc927daee5119feb122
Prajitha <98685133+prajithas@users.noreply.github.com>
2022-06-03 21:08:36 +0530

    Update BookModel.js

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,13 +1,5 @@
 const mongoose = require('mongoose');
-var username = encodeURIComponent("libraryuser");
-var password = encodeURIComponent("qJVa8A6I3X1uGIxm");
-
-var connectionString = `mongodb://${username}:${password}@cluster0.c3ku2.mongodb.net/Cluster0?retryWrites=true&w=majority`;
-
-var databaseConnection = await MongoClient.connect(connectionString, {
-    ssl: true,
-});
-//mongoose.connect('mongodb+srv://libraryuser:qJVa8A6I3X1uGIxm@cluster0.c3ku2.mongodb.net/Cluster0?retryWrites=true&w=majority');
+mongoose.connect("mongodb+srv://libraryuser:qJVa8A6I3X1uGIxm@cluster0.c3ku2.mongodb.net/Cluster0?retryWrites=true&w=majority");
 const Schema = mongoose.Schema;
 
 
